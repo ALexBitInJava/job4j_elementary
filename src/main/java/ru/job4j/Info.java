9f9c6f86ebac1ccc88615d78d4983deb9048f03a
ALexBitInJava <sasha.golovin.javarush@gmail.com>
2022-05-15 00:48:56 +0300

    Моя вторая программа

---
+++ src/main/java/ru/job4j/Info.java
@@ -0,0 +1,13 @@
+package ru.job4j;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+public class Info {
+    public static void main(String[] args){
+        Date date= new Date();
+        SimpleDateFormat sDF = new SimpleDateFormat("MM.dd.yyyy");
+        String test = sDF.format(date);
+        System.out.println(test);
+    }
+}
