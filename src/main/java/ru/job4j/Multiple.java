2c5c593bd916c187e701547f743708780dbb5be8
ALexBitInJava <sasha.golovin.javarush@gmail.com>
2022-05-15 00:20:21 +0300

    Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/Multiple.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+public class Multiple {
+    public static void main(String[] args){
+        int[] array = new int[10];
+        for (int i = 1;i< array.length;i++){
+            System.out.println("1 * "+ i + " = "+ i);
+        }
+    }
+}
