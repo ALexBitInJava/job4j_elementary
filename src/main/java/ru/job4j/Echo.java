68a2871cefc3258b731368563f5ea5c761bcde6b
ALexBitInJava <sasha.golovin.javarush@gmail.com>
2022-05-15 12:52:46 +0300

    Загрузка файлов на github [#243562 #301038]. Исправление.

--- src/main/java/ru/job4j/Echo.java
+++ src/main/java/ru/job4j/Echo.java
@@ -1,7 +1,6 @@
 package ru.job4j;
 
 public class Echo {
-    @SuppressWarnings("checkstyle:WhitespaceAround")
     public static void main(String[] args) {
 System.out.println("Я сделал это без подлядок? Проверяю");
     }
