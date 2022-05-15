cc94f0bb4f4dc1575b5b56f719ef457d6e0e3ce8
ALexBitInJava <sasha.golovin.javarush@gmail.com>
2022-05-15 19:31:22 +0300

    Расстояние между точками в системе координат [#188]

---
+++ src/main/java/ru/job4j/ru/job4j/condition/Greeting.java
@@ -0,0 +1,19 @@
+package ru.job4j.ru.job4j.condition;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        System.out.println(idea);
+        idea += "But I am a newbie.";
+        Date date = new Date();
+        SimpleDateFormat dateYear = new SimpleDateFormat("yyyy");
+        String test = dateYear.format(date);
+        //int year = Integer.parseInt(test.toString());
+        //System.out.println(year);
+        idea += test;
+        System.out.println(idea);
+    }
+}
