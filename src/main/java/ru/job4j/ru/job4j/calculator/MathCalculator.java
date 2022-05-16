50fcc67f0158f971831b2d0a9694b83a70fb59f4
ALexBitInJava <sasha.golovin.javarush@gmail.com>
2022-05-16 22:28:45 +0300

    Что такое import [#504773].

---
+++ src/main/java/ru/job4j/ru/job4j/calculator/MathCalculator.java
@@ -0,0 +1,22 @@
+package ru.job4j.ru.job4j.calculator;
+import static ru.job4j.math.MathFunction.*;
+
+public class MathCalculator {
+    public static double sumAndMultiply(double first, double second) {
+        return sum(first, second) + multiply(first, second);
+    }
+
+    public static double divAndDif(double first, double second) {
+        return division(first, second) - difference(first, second);
+    }
+
+    public static double totalAmount(double first, double second) {
+        return sum(first, second) + multiply(first, second) + division(first, second) + difference(first, second);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Результат расчета равен:" + sumAndMultiply(10, 20));
+        System.out.println("Результат расчета равен:" + divAndDif(10, 20));
+        System.out.println("Результат расчета равен:" + totalAmount(10, 20));
+    }
+}
