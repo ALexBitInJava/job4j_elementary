f02d66e6b0362be1c7b649f6a957a316494ded39
ALexBitInJava <sasha.golovin.javarush@gmail.com>
2022-05-17 21:04:29 +0300

    Исправление ошибок

--- src/test/java/ru/job4j/calculator/FitTest.java
+++ src/test/java/ru/job4j/calculator/FitTest.java
@@ -2,25 +2,24 @@ package ru.job4j.calculator;
 
 import org.junit.Assert;
 import org.junit.Test;
-import ru.job4j.calculator.Fit;
 
 public class FitTest {
 
     @Test
     public void whenMan180Then92() {
-    short in = 180;
-    double expected = 92;
-    double out = Fit.manWeight(in);
-    float eps = 0.01f;
+        short in = 180;
+        double expected = 92;
+        double out = Fit.manWeight(in);
+        float eps = 0.01f;
         Assert.assertEquals(expected, out, eps);
     }
 
     @Test
     public void whenWoman170Then69() {
-    short in = 170;
-    double expected = 69;
-    double out = Fit.womanWeight(in);
-    float eps = 0.01f;
-    Assert.assertEquals(expected, out, eps);
+        short in = 170;
+        double expected = 69;
+        double out = Fit.womanWeight(in);
+        float eps = 0.01f;
+        Assert.assertEquals(expected, out, eps);
     }
 }
