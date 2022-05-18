86c17443eee74418fedd87b4564306162cd2e9c8
ALexBitInJava <sasha.golovin.javarush@gmail.com>
2022-05-18 23:04:13 +0300

    Подсчет суммы чётных чисел в диапазоне [#192]

--- src/main/java/ru/job4j/loop/LoopFor.java
+++ src/main/java/ru/job4j/loop/LoopFor.java
@@ -3,11 +3,12 @@ package ru.job4j.loop;
 public class LoopFor {
     public static void main(String[] args) {
         int i;
-        boolean result1121;
-        for (i = 5; result1121 = i <= 10; i++) {
+        boolean result;
+        for (i = 5; i <= 10; i++) {
             System.out.println(i);
         }
+        result = false;
         System.out.println("Индекс после цикла равен: " + i);
-        System.out.println("Условие выполнения цикла равно: " + result1121);
+        System.out.println("Условие выполнения цикла равно: " + result);
     }
 }
