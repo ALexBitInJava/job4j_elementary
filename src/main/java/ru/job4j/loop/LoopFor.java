534fcc9026a79410fdace71a005ce991a01b5bd2
ALexBitInJava <sasha.golovin.javarush@gmail.com>
2022-05-18 23:50:27 +0300

    Создать программу, вычисляющую факториал. [#193]

--- src/main/java/ru/job4j/loop/LoopFor.java
+++ src/main/java/ru/job4j/loop/LoopFor.java
@@ -3,10 +3,12 @@ package ru.job4j.loop;
 public class LoopFor {
     public static void main(String[] args) {
         int i;
+        boolean result = true;
         for (i = 5; i <= 10; i++) {
             System.out.println(i);
+            result = false;
         }
         System.out.println("Индекс после цикла равен: " + i);
-        System.out.println("Условие выполнения цикла равно: " + false);
+        System.out.println("Условие выполнения цикла равно: " + result);
     }
 }
