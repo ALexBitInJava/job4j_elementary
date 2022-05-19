7e2578a3b28303fafd23799b1109db62839bcd11
ALexBitInJava <sasha.golovin.javarush@gmail.com>
2022-05-19 20:30:13 +0300

    Цикл for. Определение. [#3572 #302431]

--- src/main/java/ru/job4j/calculator/Converter.java
+++ src/main/java/ru/job4j/calculator/Converter.java
@@ -2,19 +2,18 @@ package ru.job4j.calculator;
 
 public class Converter {
     public static float rubleToEuro(float value) {
-        float rsl = value / 60;
-        return rsl;
+        return value / 60;
     }
 
     public static float rubleToDollar(float value) {
-        float rsl = value / 70;
-        return rsl;
+        return value / 70;
     }
 
     public static void main(String[] args) {
         float euro = Converter.rubleToEuro(140);
         String roundEuro = String.format("%.2f", euro);
         System.out.println("140 rubles are " + roundEuro + " euro.");
+
         float dollar = Converter.rubleToDollar(180);
         String roundDollar = String.format("%.2f", dollar);
         System.out.println("180 rubles are " + roundDollar + " dollar.");
