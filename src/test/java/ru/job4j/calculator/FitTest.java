006e367f151e5fc91ba66e240dd4be7368c21402
ALexBitInJava <sasha.golovin.javarush@gmail.com>
2022-05-19 20:00:43 +0300

    Тесты для задачи "идеальный вес" [#159458 #301777]

--- src/test/java/ru/job4j/calculator/FitTest.java
+++ src/test/java/ru/job4j/calculator/FitTest.java
@@ -5,4 +5,21 @@ import org.junit.Test;
 
 public class FitTest {
 
+    @Test
+    public void whenMan180Then92() {
+        short in = 180;
+        double expected = 92;
+        double out = Fit.manWeight(in);
+        float eps = 0.01f;
+        Assert.assertEquals(expected, out, eps);
+    }
+
+    @Test
+    public void whenWoman170Then69() {
+        short in = 170;
+        double expected = 69;
+        double out = Fit.womanWeight(in);
+        float eps = 0.01f;
+        Assert.assertEquals(expected, out, eps);
+    }
 }
