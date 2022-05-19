03a90cd54c0aabf0670d2f4edcd8c81e436f994a
ALexBitInJava <sasha.golovin.javarush@gmail.com>
2022-05-19 20:12:32 +0300

    Тесты для определения расстояния между точками. [#159459 #301785]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -5,4 +5,53 @@ import org.junit.Test;
 
 public class PointTest {
 
+    @Test
+    public void when00to20then2() {
+        int x1 = 0;
+        int x2 = 0;
+        int y1 = 2;
+        int y2 = 0;
+        double expected = 2;
+        double out = Point.distance(x1, x2, y1, y2);
+        float eps = 0.01f;
+        Assert.assertEquals(expected, out, eps);
+    }
+
+    @Test
+    public void when12to34then1dot4142135623730951() {
+        int x1 = 1;
+        int x2 = 2;
+        int y1 = 3;
+        int y2 = 4;
+        double expected = 1.41;
+        double out = Point.distance(x1, x2, y1, y2);
+        float eps = 0.01f;
+        Assert.assertEquals(expected, out, eps);
+    }
+
+    @Test
+    public void when41to42then3dot605551275463989() {
+        int x1 = 4;
+        int x2 = 1;
+        int y1 = 4;
+        int y2 = 2;
+        double expected = 3.61;
+        double out = Point.distance(x1, x2, y1, y2);
+        float eps = 0.01f;
+        Assert.assertEquals(expected, out, eps);
+
+    }
+
+    @Test
+    public void when20to20then2dot8284271247461903() {
+        int x1 = 2;
+        int x2 = 0;
+        int y1 = 2;
+        int y2 = 0;
+        double expected = 2.83;
+        double out = Point.distance(x1, x2, y1, y2);
+        float eps = 0.01f;
+        Assert.assertEquals(expected, out, eps);
+    }
+
 }
