385b9cbbdfd1028e7c27011a08ea4af3bfbd147b
ALexBitInJava <sasha.golovin.javarush@gmail.com>
2022-05-20 20:30:12 +0300

    Заголовок в файле java [#159036 #301071]

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -3,5 +3,10 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Info {
-
+    public static void main(String[] args) {
+        Date date = new Date();
+        SimpleDateFormat sDF = new SimpleDateFormat("MM.dd.yyyy");
+        String test = sDF.format(date);
+        System.out.println(test);
+    }
 }
