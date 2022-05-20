0949699d95c3a120f3d389d2b5f281dfc7154374
ALexBitInJava <sasha.golovin.javarush@gmail.com>
2022-05-20 20:55:37 +0300

    Конвертер валюты [#41583 #301176]

--- src/main/java/ru/job4j/calculator/Converter.java
+++ src/main/java/ru/job4j/calculator/Converter.java
@@ -10,13 +10,6 @@ public class Converter {
     }
 
     public static void main(String[] args) {
-        float euro = Converter.rubleToEuro(140);
-        String roundEuro = String.format("%.2f", euro);
-        System.out.println("140 rubles are " + roundEuro + " euro.");
 
-        float dollar = Converter.rubleToDollar(180);
-        String roundDollar = String.format("%.2f", dollar);
-        System.out.println("180 rubles are " + roundDollar + " dollar.");
-
-    }
+}
 }
