23e02499336927436c122a350847706e02425cfc
ALexBitInJava <sasha.golovin.javarush@gmail.com>
2022-05-20 16:56:16 +0300

    Простое число [#171694 #302654]

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -2,19 +2,11 @@ package ru.job4j.loop;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        if (number <= 1) {
-            return false;
-        } else {
             for (int i = 2; i < number; i++) {
                 if (number % i == 0) {
                     return false;
                 }
             }
-        }
-        return true;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(check(1));
+        return number > 1;
     }
 }
