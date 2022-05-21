b3f78cdc0f77b2b005bb93ad8c320a7b4dfcaa0d
ALexBitInJava <sasha.golovin.javarush@gmail.com>
2022-05-21 14:27:07 +0300

    Слово начинается с ... [#41585]

---
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+
+import java.sql.PreparedStatement;
+
+public class ArrayChar {
+    public static boolean startsWith(char[] word, char[] pref) {
+        boolean result = true;
+        for (int i = 0; i < word.length; i++) {
+            for (int k = 0; k < pref.length; k++) {
+                if (!(pref[k] == word[k])) {
+                    result = false;
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+}
