bfd0418a98833096dd6ab4e2df33b2b92d7ad864
ALexBitInJava <sasha.golovin.javarush@gmail.com>
2022-05-25 00:35:18 +0300

    Рефакторинг - Расстояние между точками

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -7,36 +7,34 @@ public class PointTest {
 
     @Test
     public void when00to20then2() {
-        int x1 = 0;
-        int x2 = 0;
-        int y1 = 2;
-        int y2 = 0;
+        Point a = new Point(0, 2);
+        Point b = new Point(0, 0);
         double expected = 2;
-        double out = Point.distance(x1, x2, y1, y2);
+        double out = a.distance(b);
         float eps = 0.01f;
         Assert.assertEquals(expected, out, eps);
     }
 
     @Test
     public void when12to34then1dot41() {
-        int x1 = 1;
-        int x2 = 2;
-        int y1 = 3;
-        int y2 = 4;
+        Point a = new Point(1, 3);
+        Point b = new Point(2, 4);
         double expected = 1.41;
-        double out = Point.distance(x1, x2, y1, y2);
+        double out = a.distance(b);
         float eps = 0.01f;
         Assert.assertEquals(expected, out, eps);
     }
 
     @Test
     public void when41to42then3dot61() {
+        Point a = new Point(4, 4);
+        Point b = new Point(1, 2);
         int x1 = 4;
         int x2 = 1;
         int y1 = 4;
         int y2 = 2;
         double expected = 3.61;
-        double out = Point.distance(x1, x2, y1, y2);
+        double out = a.distance(b);
         float eps = 0.01f;
         Assert.assertEquals(expected, out, eps);
 
@@ -44,12 +42,14 @@ public class PointTest {
 
     @Test
     public void when20to20then2dot83() {
+        Point a = new Point(2, 2);
+        Point b = new Point(0, 0);
         int x1 = 2;
         int x2 = 0;
         int y1 = 2;
         int y2 = 0;
         double expected = 2.83;
-        double out = Point.distance(x1, x2, y1, y2);
+        double out = a.distance(b);
         float eps = 0.01f;
         Assert.assertEquals(expected, out, eps);
     }
