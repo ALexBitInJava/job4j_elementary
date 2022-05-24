300b5bc4631e3764df12c24602544fe5943b0029
ALexBitInJava <sasha.golovin.javarush@gmail.com>
2022-05-25 00:37:07 +0300

    Рефакторинг - Расстояние между точками

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -29,10 +29,6 @@ public class PointTest {
     public void when41to42then3dot61() {
         Point a = new Point(4, 4);
         Point b = new Point(1, 2);
-        int x1 = 4;
-        int x2 = 1;
-        int y1 = 4;
-        int y2 = 2;
         double expected = 3.61;
         double out = a.distance(b);
         float eps = 0.01f;
@@ -44,14 +40,9 @@ public class PointTest {
     public void when20to20then2dot83() {
         Point a = new Point(2, 2);
         Point b = new Point(0, 0);
-        int x1 = 2;
-        int x2 = 0;
-        int y1 = 2;
-        int y2 = 0;
         double expected = 2.83;
         double out = a.distance(b);
         float eps = 0.01f;
         Assert.assertEquals(expected, out, eps);
     }
-
 }
